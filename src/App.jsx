86f790a6d9189681b0ca661da6334b4337b99bf5
surya <cpgnsc@gmail.com>
2022-09-03 13:12:41 +0545

    37

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,26 @@ import React, { useState } from "react";
 const App = () =>{
 
 const  [name,setName] = useState("");
-
+const [Fullname, setFullName] = useState("");
 const inputEvent = (event) =>{
     console.log(event.target.value);
     setName(event.target.value);
 };
 
+const onSubmit = () =>{
+    
+    setFullName(name);
+}
     return(
   
 <>
     <div>
-        <h1>Hello  {name}</h1>
+        <h1>Hello  {Fullname}</h1>
         <input type="text" placeholder="Enter your name"
               onChange={inputEvent}
         />
 
-        <button>Click me✔  </button>
+        <button onClick={onSubmit}>Click me✔  </button>
     </div>
 </>
     );
