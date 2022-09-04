0d5a13da44b91d8c920bfa1a2b3c076f0f717e9b
surya <cpgnsc@gmail.com>
2022-09-04 14:19:19 +0545

    __|37|__

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ const  [fullName, setFullName] = useState({
 
     fname: "",
     lname: "",
+    email: "",
+    Phone: ""
 });
 
    
@@ -14,21 +16,44 @@ const inputEvent = (event) =>{
     console.log(event.target.value);
     console.log(event.target.name);
     
-    const value =event.target.value;
-    const name =event.target.name;
+    //const value =event.target.value;
+    //const name =event.target.name;
 
+    const [value,name] = event.target;
     setFullName((prevValue) => {
         //console.log(prevValue);
         if(name === "fName"){
             return{
-            fname: value,
-            lname: prevValue.lname,
+              fname: value,
+               lname: prevValue.lname,
+               email:prevValue.email,
+               phone:prevValue.phone,
 };
         }else if(name === "lName")
         {
                 return{
                 fname: prevValue.fname,
                 lname: value,
+                email:prevValue.email,
+                phone:prevValue.phone,
+    };
+
+       }else if(name === "email")
+       {
+                return{
+                fname: prevValue.fname,
+                lname: prevValue.lname,
+                email: value,
+                phone: prevValue.phone,
+    };
+
+        }else if(name === "phone")
+        {
+                return{
+                fname: prevValue.fname,
+                lname: prevValue.lname,
+                email:prevValue.email,
+                phone:value,
     };
 
         }
@@ -50,11 +75,15 @@ const onSubmits = (event) =>{
         <h1>
         Hello  {fullName.fname} {fullName.lname}
         </h1>
-        <input type="text" 
+        <p>{fullName.email}</p>
+        <p>{fullName.phone}</p>
+        
+        <input 
+          type="text" 
            placeholder="Enter your First Name"
            name="fName"
              onChange={inputEvent} 
-             //value={fullName.fname} 
+             value={fullName.fname} 
 
              />
 
@@ -64,7 +93,22 @@ const onSubmits = (event) =>{
           placeholder="Enter your Last Name"
           name="lName"
           onChange = {inputEvent}
-         // value = {fullName.lname} 
+          value = {fullName.lname} 
+         />
+
+        <input type="email" 
+          placeholder="Enter your Email"
+          name="email"
+          onChange = {inputEvent}
+          value = {fullName.email} 
+          autoCapitalize="off"
+         />
+
+        <input type="number" 
+          placeholder="Enter your Number"
+          name="phone"
+          onChange  = {inputEvent}
+          value = {fullName.phone} 
          />
 
         <button type="submit">Click me✔</button>
